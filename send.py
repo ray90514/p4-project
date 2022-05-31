082048831da80298d92275d66db2ef315cc46089
lyc <richie900421@gmail.com>
2022-05-31 03:10:14 +0000

    Update py

--- send.py
+++ send.py
@@ -1,40 +1,55 @@
 #!/usr/bin/env python3
-import argparse
-import sys
-import socket
-import random
-import struct
 
-from scapy.all import sendp, send, get_if_list, get_if_hwaddr
-from scapy.all import Packet
+import argparse
+import socket
+
+from scapy.all import sendp, send, hexdump, get_if_list, get_if_hwaddr
 from scapy.all import Ether, IP, UDP, TCP
 
+from time import sleep
+
+
 def get_if():
-    ifs=get_if_list()
-    iface=None # "h1-eth0"
+    iface = None
     for i in get_if_list():
         if "eth0" in i:
-            iface=i
-            break;
+            iface = i
+            break
     if not iface:
         print("Cannot find eth0 interface")
         exit(1)
     return iface
 
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--p", help="Protocol name To send TCP/UDP etc packets", type=str)
+    parser.add_argument("--des", help="IP address of the destination", type=str)
+    parser.add_argument("--m", help="Raw Message", type=str)
+    parser.add_argument("--dur", help="in seconds", type=str)
+    args = parser.parse_args()
 
-    if len(sys.argv)<3:
-        print('pass 2 arguments: <destination> "<message>"')
-        exit(1)
-
-    addr = socket.gethostbyname(sys.argv[1])
-    iface = get_if()
-
-    print(("sending on interface %s to %s" % (iface, str(addr))))
-    pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
-    pkt = pkt /IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / sys.argv[2]
-    pkt.show2()
-    sendp(pkt, iface=iface, verbose=False)
+    if args.p and args.des and args.m and args.dur:
+        addr = socket.gethostbyname(args.des)
+        iface = get_if()
+        if args.p == 'UDP':
+            pkt = Ether(src=get_if_hwaddr(iface), dst="ff:ff:ff:ff:ff:ff") / IP(dst=addr, tos=1) / UDP(dport=4321, sport=1234) / args.m
+            pkt.show2()
+            try:
+                for i in range(int(args.dur)):
+                    sendp(pkt, iface=iface)
+                    sleep(1)
+            except KeyboardInterrupt:
+                raise
+        elif args.p == 'TCP':
+            pkt = Ether(src=get_if_hwaddr(iface), dst="ff:ff:ff:ff:ff:ff") / IP(dst=addr, tos=1) / TCP() / args.m
+            pkt.show2()
+            try:
+                for i in range(int(args.dur)):
+                    sendp(pkt, iface=iface)
+                    sleep(1)
+            except KeyboardInterrupt:
+                raise
 
 
 if __name__ == '__main__':
